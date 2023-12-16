6ce741943d40a9c980ca9dbfb7b24d8938af2eb1
suzy1031 <kohei_sugawara@hotmail.co.jp>
2023-12-17 07:51:41 +0900

    Add vitest.config.ts for Vite testing configuration

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  test: {
+    include: ["src/tests/**/*.test.tsx", "src/tests/**/*.test.ts"],
+    environment: "jsdom",
+    coverage: {
+      reporter: ["html"],
+      reportsDirectory: "./coverage",
+    },
+    // これを有効にしないとexpect関数でエラーする
+    // ReferenceError: expect is not defined
+    globals: true,
+  },
+});
