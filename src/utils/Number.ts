22f9887800b71716ec5cddcbbc9fff7dd5cce128
suzy1031 <kohei_sugawara@hotmail.co.jp>
2023-12-29 07:11:32 +0900

    Add plus and minus functions to Number.ts

--- src/utils/Number.ts
+++ src/utils/Number.ts
@@ -1,4 +1,7 @@
-const plus = (v1: number, v2: number) => {
+export const plus = (v1: number, v2: number) => {
   return v1 + v2;
 };
-export default plus;
+
+export const minus = (v1: number, v2: number) => {
+  return v1 - v2;
+};
