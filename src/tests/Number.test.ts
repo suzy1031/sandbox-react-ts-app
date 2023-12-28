f32ce02f07ac17b975babbd148ff175b0979c389
suzy1031 <kohei_sugawara@hotmail.co.jp>
2023-12-29 07:11:37 +0900

    Add minus function to Number.test.ts

--- src/tests/Number.test.ts
+++ src/tests/Number.test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from "vitest";
-import plus from "../utils/Number";
+import { minus, plus } from "../utils/Number";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(plus(1, 2)).toBe(3);
+});
+
+test("subtracts 2 - 1 to equal 1", () => {
+  expect(minus(2, 1)).toBe(1);
 });
