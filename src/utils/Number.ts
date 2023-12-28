c34c581cee24349602d10315203b862d0a478e61
suzy1031 <kohei_sugawara@hotmail.co.jp>
2023-12-29 08:50:42 +0900

    Add divide function to Number.ts

--- src/utils/Number.ts
+++ src/utils/Number.ts
@@ -9,3 +9,10 @@ export const minus = (v1: number, v2: number) => {
 export const multi = (a: number, b: number): number => {
   return a * b;
 };
+
+export const divide = (a: number, b: number): number => {
+  if (b === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return a / b;
+};
