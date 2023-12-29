1def1615dd26fdef078098837963f2aed0d3787b
suzy1031 <kohei_sugawara@hotmail.co.jp>
2023-12-30 06:19:57 +0900

    Update typography font size and family

--- src/libs/theme.ts
+++ src/libs/theme.ts
@@ -2,6 +2,8 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
   typography: {
+    fontSize: 13,
+    fontFamily: `'Noto Sans JP', 'Yu Gothic', YuGothic, 'ヒラギノ角ゴ Pro W3', 'Hiragino Kaku Gothic Pro', 'メイリオ', Meiryo, Osaka, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif`,
     button: {
       fontSize: 14,
       textTransform: "none",
