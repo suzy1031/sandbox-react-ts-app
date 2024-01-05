ee4b35c78f1b0efb8d88e2f8f3de67eb3d67cb23
suzy1031 <kohei_sugawara@hotmail.co.jp>
2024-01-05 15:36:41 +0900

    Update HtmlButton.stories.tsx

--- src/stories/ui/HtmlButton.stories.tsx
+++ src/stories/ui/HtmlButton.stories.tsx
@@ -1,22 +1,22 @@
-import { expect } from "@storybook/jest";
-import { Meta, StoryObj } from "@storybook/react";
-import HtmlButton from "../../ui/HtmlButton";
-import { within, userEvent } from "@storybook/testing-library";
-import { jest } from "@storybook/jest";
+import { expect } from '@storybook/jest';
+import { Meta, StoryObj } from '@storybook/react';
+import HtmlButton from '../../ui/HtmlButton';
+import { within, userEvent } from '@storybook/testing-library';
+import { jest } from '@storybook/jest';
 
 export default {
-  title: "ui/HtmlButton",
+  title: 'ui/HtmlButton',
   component: HtmlButton,
   parameters: {
     docs: {
       description: {
         component:
-          "HtmlButton is a customizable button component for user interactions. You can set its variant, color, click behavior, size, and shape. Customize it further with the `sx` prop for specific style overrides.",
+          'HtmlButton is a customizable button component for user interactions. You can set its variant, color, click behavior, size, and shape. Customize it further with the `sx` prop for specific style overrides.',
       },
     },
   },
   argTypes: {
-    label: { description: "The text that appears on the button." },
+    label: { description: 'The text that appears on the button.' },
     variant: {
       description:
         'The variant to use. It determines the button\'s style. Examples include "contained" for a filled background or "outlined" for a bordered style.',
@@ -26,16 +26,16 @@ export default {
         'The color of the button. Typically includes "primary", "secondary", or other theme-based colors.',
     },
     handleClick: {
-      description: "Function to call when the button is clicked.",
+      description: 'Function to call when the button is clicked.',
     },
-    dataQa: { description: "A data-attribute for QA testing purposes." },
-    height: { description: "The height of the button." },
+    dataQa: { description: 'A data-attribute for QA testing purposes.' },
+    height: { description: 'The height of the button.' },
     isRounded: {
-      description: "If true, the button will have rounded corners.",
+      description: 'If true, the button will have rounded corners.',
     },
     sx: {
       description:
-        "Custom styles to apply to the button. Accepts theme-based style object.",
+        'Custom styles to apply to the button. Accepts theme-based style object.',
     },
   },
 } as Meta;
@@ -44,19 +44,19 @@ type Story = StoryObj<typeof HtmlButton>;
 
 export const PrimaryContained: Story = {
   args: {
-    label: "Primary Contained",
-    variant: "contained",
-    color: "primary",
+    label: 'Primary Contained',
+    variant: 'contained',
+    color: 'primary',
     handleClick: jest.fn(), // Mock the function
-    dataQa: "button-1",
+    dataQa: 'button-1',
     height: 48,
     isRounded: true,
-    sx: { width: "300px", fontSize: "16px" },
+    sx: { width: '300px', fontSize: '16px' },
   },
   render: (args) => <HtmlButton {...args} />,
   play: async ({ args, canvasElement }) => {
     const canvas = within(canvasElement);
-    const button = await canvas.getByText("Primary Contained");
+    const button = await canvas.getByText('Primary Contained');
 
     // Simulate user clicking the button
     await userEvent.click(button);
@@ -68,7 +68,7 @@ export const PrimaryContained: Story = {
     // Assert that handleClick has been called
     await expect(args.handleClick).toHaveBeenCalled();
   },
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   parameters: {
     docs: {
       description: {
@@ -81,18 +81,18 @@ export const PrimaryContained: Story = {
 
 export const SecondaryOutlined: Story = {
   args: {
-    label: "Secondary Outlined",
-    variant: "outlined",
-    color: "secondary",
+    label: 'Secondary Outlined',
+    variant: 'outlined',
+    color: 'secondary',
     handleClick: jest.fn(), // Mock the function,
     height: 48,
     isRounded: false,
-    sx: { width: "200px", fontSize: "14px" },
+    sx: { width: '200px', fontSize: '14px' },
   },
   render: (args) => <HtmlButton {...args} />,
   play: async ({ args, canvasElement }) => {
     const canvas = within(canvasElement);
-    const button = await canvas.getByText("Secondary Outlined");
+    const button = await canvas.getByText('Secondary Outlined');
 
     // Simulate user clicking the button
     await userEvent.click(button);
@@ -104,7 +104,7 @@ export const SecondaryOutlined: Story = {
     // Assert that handleClick has been called
     await expect(args.handleClick).toHaveBeenCalled();
   },
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   parameters: {
     docs: {
       description: {
