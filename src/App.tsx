2ef4161350b17dbafb8bcab3484f9cbbf5770f04
suzy1031 <kohei_sugawara@hotmail.co.jp>
2024-03-02 08:35:22 +0900

    feat: Remove unused form handling logic and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { type ReactElement, useCallback, useState, useEffect } from 'react';
+import { type ReactElement, useCallback, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
-import { Button, Divider, Stack } from '@mui/material';
+import { Button, Divider } from '@mui/material';
 import useHandleModal from './hooks/useHandleModal';
 import ChoiceModal from './ui/ChoiceModal';
 import HtmlButton from './ui/HtmlButton';
@@ -9,15 +9,6 @@ import Headline from './ui/Headline';
 import TwButton from './components/tailwindcss/TwButton';
 import DsButton from './components/daisyui/DsButton';
 import DsModal from './components/daisyui/DsModal';
-import TextForm from './ui/TextForm';
-import { useForm } from 'react-hook-form';
-import { FormSchema, type FormSchemaType } from './schema';
-import { zodResolver } from '@hookform/resolvers/zod';
-import dayjs from 'dayjs';
-import MuiDatePicker from './ui/MuiDatePicker';
-import MuiSelect from './ui/MuiSelect';
-import { checkAfter, checkBefore } from './utils/Time';
-// import { InputItem } from './ui/InputItem';
 
 const App = (): ReactElement => {
   const [count, setCount] = useState(0);
@@ -34,79 +25,6 @@ const App = (): ReactElement => {
     setDsOpen(false);
   }, []);
 
-  const {
-    watch,
-    control,
-    handleSubmit,
-    formState: { errors, isValid },
-    setError,
-  } = useForm<FormSchemaType>({
-    defaultValues: {
-      name: '',
-      startDate: dayjs(),
-      endDate: dayjs().add(7, 'day'),
-      startTime: 9,
-      endTime: 24,
-    },
-    mode: 'all',
-    resolver: zodResolver(FormSchema),
-  });
-
-  const onSubmit = (data: FormSchemaType): void => {
-    console.log(data);
-  };
-
-  const watchStartDate = watch('startDate');
-  const watchEndDate = watch('endDate');
-  const watchStartTime = watch('startTime');
-  const watchEndTime = watch('endTime');
-  const isBefore = checkBefore(
-    watchStartDate,
-    watchEndDate,
-    watchStartTime,
-    watchEndTime,
-  );
-  const isAfter = checkAfter(
-    watchEndDate,
-    watchStartDate,
-    watchEndTime,
-    watchStartTime,
-  );
-  const startError = errors.startDate?.message ?? errors.startTime?.message;
-  const endError = errors.endDate?.message ?? errors.endTime?.message;
-
-  /**
-   * refs:
-   * https://react-hook-form.com/docs/useform/seterror
-   * https://react-hook-form.com/docs/useform/register#registerRef
-   * https://github.com/t-shiratori/hello-zod
-   * RHFのregisterには､deps: string[]を渡すことで､他のフィールドの値を監視することができる
-   *
-   * ex)
-   * deps: ['startDate', 'endDate']
-   * -> step1: update startDate startDate > endDate -> validate startDate error
-   * -> step2: after update endDate startDate < endDate -> remove startDate error
-   *
-   * しかし､Controllerにはdepsがないため､startDateとendDateが相互に監視できない
-   * そこで､useEffectとwatchを使って監視する
-   */
-  useEffect(() => {
-    const initError = (): void => {
-      setError('startDate', {});
-      setError('endDate', {});
-      setError('startTime', {});
-      setError('endTime', {});
-    };
-
-    if (watchStartDate < watchEndDate) {
-      initError();
-    }
-
-    if (isBefore || isAfter) {
-      initError();
-    }
-  }, [isAfter, isBefore, setError, watchEndDate, watchStartDate]);
-
   return (
     <>
       <div>
@@ -114,66 +32,7 @@ const App = (): ReactElement => {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <Headline title="Sample Form" variant="xl" />
       <div className="card">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextForm name="name" control={control} errors={errors} />
-          <br />
-          <Stack direction="row" spacing={2} sx={{ my: 2 }}>
-            <MuiDatePicker
-              name="startDate"
-              control={control}
-              label="開始日"
-              errorMessage={startError}
-            />
-            <MuiSelect
-              name="startTime"
-              control={control}
-              label="開始時刻"
-              errorMessage={startError}
-            />
-
-            <MuiDatePicker
-              name="endDate"
-              control={control}
-              label="終了日"
-              errorMessage={endError}
-            />
-            <MuiSelect
-              name="endTime"
-              control={control}
-              label="終了時刻"
-              errorMessage={endError}
-            />
-          </Stack>
-          {/* <InputItem
-            register={register}
-            label={'出品開始日'}
-            fieldName={'startDate'}
-            type={'date'}
-            deps={['startDate', 'endDate']}
-            errorMessage={errors.startDate?.message}
-          /> */}
-
-          {/* <InputItem
-            register={register}
-            label={'出品終了日'}
-            fieldName={'endDate'}
-            type={'date'}
-            deps={['startDate', 'endDate']}
-            errorMessage={errors.endDate?.message}
-          /> */}
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            sx={{ my: 2 }}
-            fullWidth
-            disabled={!isValid}
-          >
-            送信
-          </Button>
-        </form>
         <Divider sx={{ my: 3 }} />
         <Headline title="Vite + React" variant="xl" />
         <button
---
+++ src/routes/sample-form.tsx
@@ -0,0 +1,11 @@
+import { createRoute } from '@tanstack/react-router';
+import { rootRoute } from './__root';
+import Form from '../components/react-hook-form/Form';
+
+export const sampleFormRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: 'sample-form',
+  component: function SampleFormComponent() {
+    return <Form />;
+  },
+});
