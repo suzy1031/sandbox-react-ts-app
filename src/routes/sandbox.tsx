399e038ce3838b7cd60c226e7734a41d311ccff9
suzy1031 <kohei_sugawara@hotmail.co.jp>
2024-03-02 08:36:19 +0900

    refactor: Simplify sandboxRoute component

--- src/routes/sandbox.tsx
+++ src/routes/sandbox.tsx
@@ -1,18 +1,11 @@
 import { createRoute } from '@tanstack/react-router';
 import { rootRoute } from './__root';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import theme from '../libs/theme';
 import App from '../App';
 
 export const sandboxRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: 'sandbox',
   component: function SandboxComponent() {
-    return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    );
+    return <App />;
   },
 });
