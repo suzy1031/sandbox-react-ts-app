8e273a0b001e96b290e3607dbc0342272aa75c82
suzy1031 <kohei_sugawara@hotmail.co.jp>
2024-03-02 08:09:51 +0900

    feat: Add TanStackRouterVite plugin to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,18 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 export default defineConfig({
   test: {
-    include: ["src/tests/**/*.test.tsx", "src/tests/**/*.test.ts"],
-    environment: "happy-dom",
+    include: ['src/tests/**/*.test.tsx', 'src/tests/**/*.test.ts'],
+    environment: 'happy-dom',
     coverage: {
-      reporter: ["text", "json-summary", "json"],
-      reportsDirectory: "./coverage/vitest",
+      reporter: ['text', 'json-summary', 'json'],
+      reportsDirectory: './coverage/vitest',
     },
     // これを有効にしないとexpect関数でエラーする
     // ReferenceError: expect is not defined
     globals: true,
   },
+  plugins: [TanStackRouterVite()], // ref: https://tanstack.com/router/latest/docs/framework/react/quick-start
 });
