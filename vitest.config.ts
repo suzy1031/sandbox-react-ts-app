e6a5d5a749d081b1bdd8ea4b5ce4b136d55d71a6
suzy1031 <kohei_sugawara@hotmail.co.jp>
2024-03-16 08:39:09 +0900

    Update vitest.config.ts to include all test files in coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,11 +7,12 @@ export default defineConfig({
     host: true,
   },
   test: {
-    include: ['src/tests/**/*.test.tsx', 'src/tests/**/*.test.ts'],
+    include: ['src/**/*.test.{js,ts,jsx,tsx}'],
     environment: 'happy-dom',
     coverage: {
       reporter: ['text', 'json-summary', 'json'],
       reportsDirectory: './coverage/vitest',
+      all: false, // これをtrueにすると全てのファイルがカバレッジレポートに表示される
     },
     // これを有効にしないとexpect関数でエラーする
     // ReferenceError: expect is not defined
